86eb17a84d061d9377229a24afdf7256d7cce34f
Pascal Martineau <pascal@lewebsimple.ca>
2022-06-24 11:43:06 -0400

    feat: Better layout with footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,12 @@
 <template>
-  <div id="default-layout" class="container py-3">
-    <slot />
+  <div id="default-layout" class="min-h-screen flex flex-col">
+    <main class="flex-grow container py-3">
+      <slot />
+    </main>
+    <footer class="bg-neutral-200 text-neutral-700 py-1.5">
+      <div class="container flex justify-end items-center gap-3">
+        <LayoutVersion class="text-sm" />
+      </div>
+    </footer>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,5 @@ definePageMeta({ title: "Home" });
 <template>
   <div id="home">
     <h1>nuxt3-prisma-graphql</h1>
-    <LayoutVersion />
   </div>
 </template>
